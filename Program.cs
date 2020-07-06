3e8b58ff8273423f48d51dd9124307f20435d6c4
Sxznnn <ramadhan.saputra@students.amikom.ac.id>
2020-07-06 17:34:28 +0700

    responsi.cs

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    class Program
+    {
+        static List<Produk> daftarProduk = new List<Produk>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS MataKuliah Pemrograman";
+            bool loop = true;
+            while (loop)
+            {
+                TampilMenu();
+
+                Console.Write("\nPilih Menu [1..4]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahProduk();
+                        break;
+
+                    case 2:
+                        HapusProduk();
+                        break;
+
+                    case 3:
+                        TampilProduk();
+                        break;
+
+                    case 4:
+                        loop = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Maaf, Menu yang Anda Pilih Tidak Ada");
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Pilih Menu Aplikasi");
+            Console.WriteLine("\n1. Tambah Produk");
+            Console.WriteLine("2. Hapus Produk");
+            Console.WriteLine("3. Tampilkan Jenis Produk");
+            Console.WriteLine("4. Keluar");
+        }
+
+        static void TambahProduk()
+        {
+            Console.Clear();
+
+            Produk produk = new Produk();
+            Console.WriteLine("Tambah Data Produk");
+            Console.Write("\nKode Produk : ");
+            produk.KodeProduk = Console.ReadLine();
+            Console.Write("Nama Produk : ");
+            produk.NamaProduk = Console.ReadLine();
+            Console.Write("Harga Pembelian : ");
+            produk.HargaBeli = double.Parse(Console.ReadLine());
+            Console.Write("Harga Untuk Dijual : ");
+            produk.HargaJual = double.Parse(Console.ReadLine());
+
+            daftarProduk.Add(produk);
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void HapusProduk()
+        {
+            Console.Clear();
+
+            int no = -1, hapus = -1;
+            Console.WriteLine("Hapus Data Produk");
+            Console.Write("Kode Produk : ");
+            string kode = Console.ReadLine();
+            foreach (Produk produk in daftarProduk)
+            {
+                no++;
+                if (produk.KodeProduk == kode)
+                {
+                    hapus = no;
+                }
+            }
+            if (hapus != -1)
+            {
+                daftarProduk.RemoveAt(hapus);
+                Console.WriteLine("\nData Produk Telah Dihapus");
+            }
+            else
+            {
+                Console.WriteLine("\nKode Produk Tidak Valid");
+            }
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke Menu");
+            Console.ReadKey();
+        }
+
+        static void TampilProduk()
+        {
+            Console.Clear();
+
+            int noUrut = 0;
+            Console.WriteLine("Daftar Produk");
+            foreach (Produk produk in daftarProduk)
+            {
+                noUrut++;
+                Console.WriteLine("{0}. Kode Produk: {1}, Nama Produk: {2}, Harga Beli: {3}, Harga Jual: {4}", noUrut, produk.KodeProduk, produk.NamaProduk, produk.HargaBeli, produk.HargaJual);
+            }
+            if (noUrut < 1)
+            {
+                Console.WriteLine("Data Produk Kosong");
+            }
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        public string KodeProduk { get; set; }
+        public string NamaProduk { get; set; }
+        public double HargaBeli { get; set; }
+        public double HargaJual { get; set; }
+
+    }
+}
